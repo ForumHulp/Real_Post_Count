4d7dffd8774c724a8a5bd543eb269433c67b1328
John <info@forumhulp.com>
2014-07-16 08:58:50 +0200

    Language typo

--- language/en/real_postcount_common.php
+++ language/en/real_postcount_common.php
@@ -18,8 +18,8 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-    'POST_COUNT' 			=> 'Raw counting posts',
-	'POST_COUNT_EXPLAIN'	=> 'This extension will count new posts since the day it\'s enabled and gie you the opportunity to add en random number of posts here.',
+    'POST_COUNT' 			=> 'Real counting posts',
+	'POST_COUNT_EXPLAIN'	=> 'This extension will count new posts since the day it\'s enabled and give you the opportunity to add en random number of posts here.',
 	'POST_COUNT_REAL'		=> 'Overall our users have posted: <strong>%1$s</strong> posts on this board.<br />',
 	'TOTAL_POSTS_COUNTER'	=> 'Currently <strong>%d</strong> posts'
 ));
